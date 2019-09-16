6e6b11c75bb2a7f5848afb85e170b4e53f0be766
fanlychie <fanlychie@yeah.net>
2019-09-16 16:49:06 +0800

    add unit testing

--- springboot-fileupload/src/test/java/org/fanlychie/fileupload/sample/test/FileUploadApplicationTest.java
+++ springboot-fileupload/src/test/java/org/fanlychie/fileupload/sample/test/FileUploadApplicationTest.java
@@ -1,13 +1,34 @@
 package org.fanlychie.fileupload.sample.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class FileUploadApplicationTest {
 
+    @Autowired
+    private MockMvc mvc;
 
+    @Test
+    public void testFileupload() throws Throwable {
+        mvc.perform(multipart("/fileupload")
+                .file(new MockMultipartFile("files", "test.txt",
+                        MediaType.MULTIPART_FORM_DATA_VALUE, "Hello World.".getBytes("UTF-8"))))
+        .andExpect(status().isOk())
+        .andDo(print())
+        ;
+    }
 
 }
